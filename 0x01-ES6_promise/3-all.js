854f799d9d9da328af2a7b59c40c20eef08cfd44
M-sharkawy <mostafasharks021@gmail.com>
2025-04-17 01:02:51 +0200

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((response) => {
-      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+    .then(([upres, usrres]) => {
+      console.log(`${upres.body} ${usrres.firstName} ${usrres.lastName}`);
     })
-    .catch(() => new Error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
