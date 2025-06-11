a3006969d9d740b033dff6df1788c5d22c9f1a00
M-sharkawy <mostafasharks021@gmail.com>
2025-06-12 01:00:02 +0300

    task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const { countStudents } = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+
+    countStudents(process.argv[2])
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Cannot load the database');
+  }
+});
+
+app.listen(port, hostname, () => {
+
+});
+
+module.exports = app;
