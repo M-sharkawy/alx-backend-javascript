2b2da4bf96411e51aed4f3b6c0d9345193c0fc94
M-sharkawy <mostafasharks021@gmail.com>
2025-06-11 21:31:31 +0300

    task 1 stdin from the terminal

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdout.write("Welcome to ALX, what is your name?"+ '\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', data => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', data => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+    process.exit();
+  });
+
+  process.on('exit', () => {
+    console.log(`This important software is now closing\n`);
+  });
+}
